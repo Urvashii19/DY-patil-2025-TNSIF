c3b5d80464b52ad34523e838a1cbbb3e49bda80f
urvas <urvas@LAPTOP-HLJ6VGPA>
2025-07-04 01:53:40 +0530

    Daytwo

---
+++ loopsDemo/src/loopsDemo/ForLoopsDemo.java
@@ -0,0 +1,14 @@
+package loopsDemo;
+
+public class ForLoopsDemo {
+	
+	public static void main(String[] args) {
+
+		for(int i=1; i<=100 ; i++)
+	
+		{
+			System.out.print("value of i :"+ i + "  ");
+		}
+	}
+
+}
