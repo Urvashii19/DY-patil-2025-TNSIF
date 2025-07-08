562b01813a5098f6d8f9afc53111760ad8e445d5
urvas <urvas@LAPTOP-HLJ6VGPA>
2025-07-08 13:31:16 +0530

    dayEight

--- daysevenoverloading/src/daysevenoverloading/methodoverloading.java
+++ daysevenoverloading/src/daysevenoverloading/methodoverloading.java
@@ -1,5 +1,35 @@
 package daysevenoverloading;
 
 public class methodoverloading {
-
+	
+	public static int addition(int a, int b)
+	{
+		return a+b;
+	}
+	
+	public static int addition(int a, int b, int c)
+	{
+		return a+b+c;
+	}
+	public static float addition(float a, float b)
+	{
+		return a+b;
+	}
+	public static float addition(float a, int b)
+	{
+		return a+b;
+	}
+	
+	public static float addition(int a, float b)
+	{
+		return a+b;
+	}
+	
+	
+	
+	
+	
+	
 }
+
+
