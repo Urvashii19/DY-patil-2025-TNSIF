509e0723b87d0ec58df5d8684d8fc178cc41fccf
urvas <urvas@LAPTOP-HLJ6VGPA>
2025-07-10 00:12:10 +0530

    dayeleven

---
+++ dayeleven/src/dayeleven/MultipleCatchdemo.java
@@ -0,0 +1,15 @@
+package dayeleven;
+
+public class MultipleCatchdemo {
+public static void main(String[] args) {
+		
+		System.out.println("I am in main method ");
+		
+		Division.divide();
+
+	}
+
+}
+	    
+	
+	
---
+++ dayeleven/src/dayeleven/NestedtryCatchDemo.java
@@ -0,0 +1,12 @@
+package dayeleven;
+
+public class NestedtryCatchDemo {
+public static void main(String[] args) {
+		
+		NestedtryCatch.check();
+
+	}
+
+}
+
+
