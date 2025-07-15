72f3c190c011d10595c9edb90638ed35256a3ae4
urvas <urvas@LAPTOP-HLJ6VGPA>
2025-07-16 01:23:46 +0530

    day12

---
+++ daytwelve/daytwelvemultithreading/childdemo.java
@@ -0,0 +1,18 @@
+package daytwelvemultithreading;
+
+public class childdemo {
+	
+		public static void main(String[] args) {
+			
+			ChildThread c=new ChildThread(10, "Hello");
+			ChildThread c1=new ChildThread(5, "TNSIF");
+			
+			c.start();
+			c1.start();
+			System.out.println("End of the Program");
+
+		}
+
+	}
+
+
---
+++ daytwelve/daytwelverunnable/UsingRunnable.java
@@ -0,0 +1,36 @@
+package daytwelverunnable;
+
+public class UsingRunnable {
+	
+		Thread t;
+		int end, start;
+		String msg;
+	//para constructor
+		public UsingRunnable(int end, int start, String msg) {
+			super();
+			this.end = end;
+			this.start = start;
+			this.msg = msg;
+		}
+
+		@Override
+		public void run() {
+			
+			for(int i=end; i>start; i--)
+			{
+				try {
+					Thread.sleep(500);
+					
+				}
+				catch(InterruptedException e)
+				{
+					System.err.println(e.getMessage());
+				}
+				System.out.println(i + msg);
+			}
+			
+		}
+
+	}
+
+
